f33ea4599718195765c9179a6242fd1ab40f4f7c
sdlw7757 <132865611+sdlw7757@users.noreply.github.com>
2025-05-05 21:01:12 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,31 +1,30 @@
-﻿
-="http://www.w3.org/1999/xhtml"
-<头>
-<metacontent="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" /> 
-<metahttp-equiv="Content-Type" content="text/html; charset=utf-8" />
-<标题><标题>我爱云解析</标题></标题>
-<link<link href="pucms.css" rel="stylesheet">href="pucms.css" rel="stylesheet">
-<script<script src="jquery-1.7.2.min.js" type="text/javascript"></script>src="jquery-1.7.2.min.js" type="text/javascript"></script>
-<script<script src="pucms.js" type="text/javascript"></script>src="pucms.js" type="text/javascript"></script>
-
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" /> 
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>我爱云解析</title>
+<link href="pucms.css" rel="stylesheet">
+<script src="jquery-1.7.2.min.js" type="text/javascript"></script>
+<script src="pucms.js" type="text/javascript"></script>
 </head>
-<body<body style="overflow-y:hidden;">style="overflow-y:hidden;">
-<divclass="panel">
-    【M3U8调用】<a href="dp/?url=">【M3U8调用】</a>
-    【VIP云解析】<a href="https://jx.xmflv.com/?url=">【VIP云解析】</a>
-    【DPlayer】<a href="javascript:QQ('dp/?url=<?php echo $_GET["url"]; ?>')">【DPlayer】</a> 
-    <a href="javascript:QQ('dp2/?url=<?php echo $_GET["url"]; ?>')">【DP-P2P】</a>
-    <a href="javascript:QQ('ck/?url=<?php echo $_GET["url"]; ?>')">【Ckplayer】</a>
-  	<a href="javascript:QQ('ck2/?url=<?php echo $_GET["url"]; ?>')">【CK-P2P】</a>  	
+<body style="overflow-y:hidden;">
+<div class="panel">
+    <a href="/dp/?url=">【M3U8调用】</a>
+    <a href="https://jx.xmflv.com/?url=">【VIP云解析】</a>
+    <a href="dp2/?url=">【DP-P2P】</a>
+    <a href="dp/?url=">【DPlayer】</a> 
+    <a href="ck/?url=">【CKplayer】</a>
+    <a href="ck2/?url=">【CK-P2P】</a>  	
 </div>
-<滑动>
-    切换<a class="WANG-WANG">切换</a></p>
-<div样式=
-    <iframe id===="WANG" scrolling"no" allowtransparency"true" frameborder"0"
-            src="dp2/?url=  ; =
-    <script type="text/javascript"> function QQ(url) {<script type="text/javascript"> function QQ(url) {
-            $(#WANG).attr('src', decodeURIComponent(decodeURIComponent(url))).show();$(#WANG).attr('src', decodeURIComponent(decodeURIComponent(url))).show();
-        } </脚本>} </脚本>
+<p class="slide">
+    <a class="WANG-WANG">切换</a></p>
+<div style="margin:-36px auto;width:100%;height:100%;">
+    <iframe id="WANG" scrolling="no" allowtransparency="true" frameborder="0"
+            src="dp2/?url=<?php echo $_GET["url"]; ?>"width="100%" scrolling="no" height="100%" align="middle" frameborder="no" hspace="0" vspace="0" allowFullscreen="true" marginheight="0" marginwidth="0" name="tv"></iframe>
+    <script type="text/javascript"> function QQ(url) {
+            $('#WANG').attr('src', decodeURIComponent(decodeURIComponent(url))).show();
+        } </script>
 </div>
-</主体>
+</body>
 </html>
